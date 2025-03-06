02993efb0f3dd3663695708831a67a095e556131
Colton Jefferies <cjefferies194@gmail.com>
2025-03-06 15:31:24 -0500

    Updates comments; chapter 2.

--- chapter-2.py
+++ chapter-2.py
@@ -13,7 +13,7 @@ print(message)
 
 #uuuuuuhhhhhhhhhh comment for execise?
 
-# Exercise 2-3: 
+# Exercise 2-3: personal message
 first = 'chris'
 last = 'jefferies'
 full = f"{first} {last}"
@@ -23,32 +23,32 @@ name = 'Chris Jefferies'
 message = ', this is an example string'
 message_to = f"{hello} my dear father {name}{message}."
 print(message_to)
-# 2 5
+# Exercise 2-5: famous qoute
 unknown_quote = 'An unknown person once said: "I shall either find a way or make one"'
 print(unknown_quote)
-# 2 6 
-famous_person = "An unknown person once said:"
 
+# Exercise 2-6:  famous qoute 2
+famous_person = "An unknown person once said:"
 qoute = '"I shall either find a way or make one"'
 
 print(famous_person + qoute)
-# 2 7
+# Exercise 2-7: strip names
 whitespace = ' name@gmail.com '
 print(whitespace)
 
 whitespace.strip(' name@gmail.com ')
 print(whitespace)
-# 2 8
+# Exercise 2-8: file extend
 filename = ('python_notes.txt')
 print(filename)
-#2 11
-# 2 9
+#Exercise 2-11: add comments
+# Exercise 2-9: 8
 print(1 + 7)
 print(10 - 2)
 print(16 / 2)
 print(4 * 2)
-# 2 11
-# 2 10
+# Exercise 2-11: add comments
+# Exercise 2-10: fave number
 fave_numb = (-2 + 4)
 print(fave_numb)
 
