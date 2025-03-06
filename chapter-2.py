7da3f3235a40fa6b870d7bf27898b4d0c60852d0
Colton Jefferies <cjefferies194@gmail.com>
2025-03-06 15:44:55 -0500

    whitespace stuff

--- chapter-2.py
+++ chapter-2.py
@@ -23,6 +23,7 @@ name = 'Chris Jefferies'
 message = ', this is an example string'
 message_to = f"{hello} my dear father {name}{message}."
 print(message_to)
+
 # Exercise 2-5: famous qoute
 unknown_quote = 'An unknown person once said: "I shall either find a way or make one"'
 print(unknown_quote)
@@ -38,15 +39,18 @@ print(whitespace)
 
 whitespace.strip(' name@gmail.com ')
 print(whitespace)
+
 # Exercise 2-8: file extend
 filename = ('python_notes.txt')
 print(filename)
-#Exercise 2-11: add comments
+
+# Exercise 2-11: add comments
 # Exercise 2-9: 8
 print(1 + 7)
 print(10 - 2)
 print(16 / 2)
 print(4 * 2)
+
 # Exercise 2-11: add comments
 # Exercise 2-10: fave number
 fave_numb = (-2 + 4)
