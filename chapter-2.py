6c31bc94f7391f1efc057b1d0b711362ae65becd
Colton Jefferies <cjefferies194@gmail.com>
2025-03-06 15:28:24 -0500

    Adds chapter 2.

---
+++ chapter-2.py
@@ -0,0 +1,55 @@
+
+
+# Exercise 2-1: Simple message
+message = "Hello World"
+print(message)
+
+# Exercise 2-2: Simple messages
+message = "hi"
+print(message)
+
+message = "error_1_purpose"
+print(message)
+
+#uuuuuuhhhhhhhhhh comment for execise?
+
+# Exercise 2-3: 
+first = 'chris'
+last = 'jefferies'
+full = f"{first} {last}"
+print(full)
+hello = 'Hello'
+name = 'Chris Jefferies'
+message = ', this is an example string'
+message_to = f"{hello} my dear father {name}{message}."
+print(message_to)
+# 2 5
+unknown_quote = 'An unknown person once said: "I shall either find a way or make one"'
+print(unknown_quote)
+# 2 6 
+famous_person = "An unknown person once said:"
+
+qoute = '"I shall either find a way or make one"'
+
+print(famous_person + qoute)
+# 2 7
+whitespace = ' name@gmail.com '
+print(whitespace)
+
+whitespace.strip(' name@gmail.com ')
+print(whitespace)
+# 2 8
+filename = ('python_notes.txt')
+print(filename)
+#2 11
+# 2 9
+print(1 + 7)
+print(10 - 2)
+print(16 / 2)
+print(4 * 2)
+# 2 11
+# 2 10
+fave_numb = (-2 + 4)
+print(fave_numb)
+
+
