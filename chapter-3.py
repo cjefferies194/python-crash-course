7bf801d832f361b2916c27e08ca6407ac6560bc9
Colton Jefferies <cjefferies194@gmail.com>
2025-03-11 12:29:56 -0400

    Adds chapter 3

---
+++ chapter-3.py
@@ -0,0 +1,61 @@
+# 3-1/3-2
+names = ['chris, have a good day','amanda, have also good day','colton, i am you']
+print(names)
+print(names[0])
+print(names[1])
+print(names[2])
+# 3-3
+want = ['i want to code a big minecraft mod','i want to be famous','i want to be a youtuber ']
+print(want[0])
+print(want[1])
+print(want[2])
+#3-4
+guests = ['victor','jack','amanda']
+print(guests)
+print(guests[2])
+# 3-5
+cant_come = 'amanda'
+guests.remove(cant_come)
+print(guests)
+print(f"{cant_come.title()} cannot make it")
+# 3-6
+guests.insert(0,'chris')
+guests.insert(1,'adam')
+guests.insert(4,'dominic')
+print(guests)
+# 3-7
+print('only two can come to my dinner')
+popped_guest = guests.pop(0)
+popped_guest = guests.pop(1)
+popped_guest = guests.pop(2)
+print(guests)
+# 3-9
+len(guests)
+#3-8
+woeld = ['japan', 'america','spain','mexico','russia']
+print('original list')
+print(woeld)
+print('sorted list')
+sorted(woeld)
+print(woeld)
+sorted(woeld)
+print(woeld)
+woeld.sort(reverse=True)
+print(woeld)
+woeld.sort(reverse=False)
+print(woeld)
+woeld.sort()
+print(woeld)
+woeld.sort()
+print(woeld)
+mega = ['missisipi','appalachiens','victor','martin luther','king jr', 'father wade','father wade king jr']
+popped_mega = mega.pop(6)
+mega.sort()
+mega.sort(reverse=True)
+popem = ('missisipi')
+mega.remove(popem)
+print(mega[0])
+print(mega[1])
+print(mega[2])
+print(mega[3])
+print(mega[4])
